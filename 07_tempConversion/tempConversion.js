1f198fecef252337ab1269aaa5fa3bcfae6dea57
Pranay <49142933+thepran@users.noreply.github.com>
2022-07-22 12:12:15 +0530

    updated tempearture concersion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (inFahrenheit) {
+  const inCelsius = (inFahrenheit - 32) * 5 / 9
+  return Math.round(inCelsius * 10) / 10
 };
 
-const ctof = function() {
+const ctof = function (inCelsius) {
+  const inFahrenheit = inCelsius * 9 / 5 + 32
+  return Math.round(inFahrenheit * 10) / 10
 
 };
 
@@ -11,3 +14,4 @@ module.exports = {
   ftoc,
   ctof
 };
+
